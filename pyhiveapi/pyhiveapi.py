b626417b7895fac374e05ae7b0803afda445a8db
KJ <ktech6@outlook.com>
2017-10-28 13:46:29 +0100

    Switch Code

--- pyhiveapi/pyhiveapi.py
+++ pyhiveapi/pyhiveapi.py
@@ -1322,8 +1322,166 @@ class Pyhiveapi:
 
     class Switch():
         """Hive Switches."""
-        temp_Switch = "Delete Me" #### DELETE ME #####
+        def get_state(self, node_id):
+            """Get smart plug current state."""
+            node_index = -1
 
-        def GetSessionID(self): #### DELETE ME #####
-            return HSC.session_id
+            smartplug_state_tmp = "UNKNOWN"
+            smartplug_state_return = "UNKNOWN"
+            smartplug_state_found = False
 
+            current_node_attribute = "Smartplug_State_" + node_id
+
+            if len(HSC.products.plug) > 0:
+                for current_node_index in range(0, len(HSC.products.plug)):
+                    if "id" in HSC.products.plug[current_node_index]:
+                        if HSC.products.plug[current_node_index]["id"] == node_id:
+                            node_index = current_node_index
+                            break
+
+                if node_index != -1:
+                    if ("state" in HSC.products.plug[
+                        node_index] and "status" in
+                        HSC.products.plug[node_index]["state"]):
+                        smartplug_state_tmp = (HSC.products.plug[node_index]
+                                               ["state"]["status"])
+                        smartplug_state_found = True
+
+            if smartplug_state_found:
+                NODE_ATTRIBS[current_node_attribute] = smartplug_state_tmp
+                smartplug_state_return = smartplug_state_tmp
+            else:
+                if current_node_attribute in NODE_ATTRIBS:
+                    smartplug_state_return = NODE_ATTRIBS.get(
+                        current_node_attribute)
+                else:
+                    smartplug_state_return = "UNKNOWN"
+
+            if HSC.logging:
+                _LOGGER.warning("State is %s", smartplug_state_return)
+
+            return smartplug_state_return
+
+        def get_power_usage(self, node_id):
+            """Get smart plug current power usage."""
+            node_index = -1
+
+            current_power_tmp = 0
+            current_power_return = 0
+            current_power_found = False
+
+            current_node_attribute = "Smartplug_Current_Power_" + node_id
+
+            if len(HSC.products.plug) > 0:
+                for current_node_index in range(0, len(HSC.products.plug)):
+                    if "id" in HSC.products.plug[current_node_index]:
+                        if HSC.products.plug[current_node_index]["id"] == node_id:
+                            node_index = current_node_index
+                            break
+
+                if node_index != -1:
+                    if ("props" in HSC.products.plug[node_index]
+                        and "powerConsumption"
+                        in HSC.products.plug[node_index]["props"]):
+                        current_power_tmp = (HSC.products.plug[node_index]
+                                             ["props"]["powerConsumption"])
+                        current_power_found = True
+
+            if current_power_found:
+                NODE_ATTRIBS[current_node_attribute] = current_power_tmp
+                current_power_return = current_power_tmp
+            else:
+                if current_node_attribute in NODE_ATTRIBS:
+                    current_power_return = NODE_ATTRIBS.get(
+                        current_node_attribute)
+                else:
+                    current_power_return = 0
+
+            if HSC.logging:
+                _LOGGER.warning("Power consumption is %s",
+                                current_power_return)
+            return current_power_return
+
+        def turn_on(self, node_id):
+            """Set smart plug to turn on."""
+            Pyhiveapi.check_hive_api_logon(self)
+
+            node_index = -1
+
+            set_mode_success = False
+            api_resp_d = {}
+            api_resp = ""
+
+            if HSC.session_id is not None:
+                if len(HSC.products.plug) > 0:
+                    for current_node_index in range(0, len(HSC.products.plug)):
+                        if "id" in HSC.products.plug[current_node_index]:
+                            if HSC.products.plug[current_node_index][
+                                "id"] == node_id:
+                                node_index = current_node_index
+                                break
+                    if node_index != -1:
+                        json_string_content = '{"status": "ON"}'
+                        hive_api_url = (HIVE_API.urls.nodes
+                                        + '/'
+                                        + node_device_type
+                                        + '/'
+                                        + HSC.products.plug[node_index]["id"])
+                        api_resp_d = hive_api_json_call("POST",
+                                                        hive_api_url,
+                                                        json_string_content,
+                                                        False)
+
+                        api_resp = api_resp_d['original']
+
+                    else:
+                        _LOGGER.error("Unable to control %s", node_name)
+
+                    if str(api_resp) == "<Response [200]>":
+                        hive_api_get_nodes(node_id, device_type)
+                        fire_bus_event(node_id, device_type)
+                        set_mode_success = True
+
+            return set_mode_success
+
+        def turn_off(self, node_id):
+            """Set smart plug to turn off."""
+            Pyhiveapi.check_hive_api_logon(self)
+
+            node_index = -1
+
+            set_mode_success = False
+            api_resp_d = {}
+            api_resp = ""
+
+            if HSC.session_id is not None:
+                if len(HSC.products.plug) > 0:
+                    for current_node_index in range(0, len(HSC.products.plug)):
+                        if "id" in HSC.products.plug[current_node_index]:
+                            if HSC.products.plug[current_node_index][
+                                "id"] == node_id:
+                                node_index = current_node_index
+                                break
+                    if node_index != -1:
+                        json_string_content = '{"status": "OFF"}'
+                        hive_api_url = (HIVE_API.urls.nodes
+                                        + '/'
+                                        + node_device_type
+                                        + '/'
+                                        + HSC.products.plug[node_index]["id"])
+                        api_resp_d = hive_api_json_call("POST",
+                                                        hive_api_url,
+                                                        json_string_content,
+                                                        False)
+
+                        api_resp = api_resp_d['original']
+
+                    else:
+                        _LOGGER.error("Unable to control %s", node_name)
+
+                    if str(api_resp) == "<Response [200]>":
+                        hive_api_get_nodes(node_id, device_type)
+                        fire_bus_event(node_id, device_type)
+                        set_mode_success = True
+
+            return set_mode_success
