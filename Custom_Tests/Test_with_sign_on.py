ce34dfb647a066ccf8fcf3bb256b2afd6cab255e
KJonline <ktech6@outlook.com>
2019-05-25 21:22:07 +0100

    new test

---
+++ Custom_Tests/Test_with_sign_on.py
@@ -0,0 +1,13 @@
+
+from .hive_session import Session
+
+p = Session()
+
+print('Using Sign on')
+username = (input("Enter username : ") or None)
+
+password = (input("Enter password: ") or None)
+
+
+result = p.initialise_api(username, password, 1)
+print(result)
