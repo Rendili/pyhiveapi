f2012fbb76d76e2c909fc8581882b10cab676d69
KJonline <ktech6@outlook.com>
2019-09-22 21:27:38 +0100

    Updated __init__

--- pyhiveapi/__init__.py
+++ pyhiveapi/__init__.py
@@ -1,1 +1,14 @@
 """__init__.py"""
+from pyhiveapi.action import Action
+from pyhiveapi.custom_logging import Logger
+from pyhiveapi.device_attributes import Attributes
+from pyhiveapi.heating import Heating
+from pyhiveapi.hive_api import Hive
+from pyhiveapi.hive_data import Data
+from pyhiveapi.hive_session import Session
+from pyhiveapi.hotwater import Hotwater
+from pyhiveapi.hub import Hub
+from pyhiveapi.light import Light
+from pyhiveapi.plug import Plug
+from pyhiveapi.sensor import Sensor
+from pyhiveapi.weather import Weather
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyhiveapi',
-    version='0.2.17dev27',
+    version='0.2.20dev0',
     description='A Python library to interface with the Hive API',
     long_description="A Python library to interface with the Hive API",
     url='https://github.com/Rendili/pyhiveapi',
